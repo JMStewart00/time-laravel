627b9ffeea3cf04c75a119f422d92a5908b0f782
JMStewart00 <jmstewart00@gmail.com>
2017-07-06 15:13:18 -0400

    added layouts and views, working includes

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+
+
+<footer>
+	<nav class="text-center navbar fixed-bottom d-block bg-inverse">
+		<div class="container borderXwidth p-0">
+			<div class="row">
+				<a class="col-1" href="/app/php/clients.php">Clients</a>
+				<span class="col-2" style="color: #EF767A;" ><i class="fa fa-clock-o fa-2x" aria-hidden="true"></i><h4 class="d-inline-block pl-2 pr-2 mx-auto">TASKr</h4></span>
+				<a class="col-1" href="#">Invoices</a>
+			</div>
+		</div>
+	</nav>
+</footer>
