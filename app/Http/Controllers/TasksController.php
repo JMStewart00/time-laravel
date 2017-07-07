88159bbd69084f0e702b61708c15b0039b848fa1
JMStewart00 <jmstewart00@gmail.com>
2017-07-06 20:22:45 -0400

    foreach in tasks for data, still need duration calculation - WIP

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Task;
 
 class TasksController extends Controller
 {
     public function index() 
     {
-    	return view('tasks.index');
+    	$tasks = Task::all();
+		return view('tasks.index', compact('tasks'));
     }
+
+
 }
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,3 +1,31 @@
 @extends('layouts.master')
 
+@section('content')
+
+
+<table class="table">
+	<thead>
+		<tr>
+			<th>Date</th>
+			<th>Client</th>
+			<th>Task Name</th>
+			<th>Duration</th>
+			<th></th>
+		</tr>
+	</thead>
+	<tbody>
+@foreach ($tasks as $task)
+		<tr>
+			<td>{{ $task->created_at->toFormattedDateString() }}</td>
+			<td>Need to add client</td>
+			<td>{{ $task->task_name }}</td>
+			<td></td>
+			<td> </td>
+		</tr>
+@endforeach
+	</tbody>
+</table>
+
+@endsection
+
 
